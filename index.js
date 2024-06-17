2b0932cac2d6e9a49ae7c8f7cb52e7e01de9e122
fouadbouklada <fouadtvdyn@gmail.com>
2024-06-17 22:48:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(port, () => {
 
 function keepAlive() {
   setInterval(() => {
-    axios.get('https://serveralpha-1.onrender.com/')
+    axios.get('https://server-alpha-1.onrender.com/')
       .then(response => {
         console.log('Ping to Server A successful');
       })
